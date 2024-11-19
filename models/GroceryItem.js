30dac8356e36ec7d49f21cfff73a218201f7e4ad
jploog066 <142631185+jploog066@users.noreply.github.com>
2024-11-19 19:38:35 +0000

    commit

--- models/GroceryItem.js
+++ models/GroceryItem.js
@@ -1,4 +1,7 @@
 // models/GroceryItem.js
 const mongoose = require("mongoose");
-const grocerySchema = new mongoose.Schema({});
+const grocerySchema = new mongoose.Schema({
+    item: { type: String },
+    food_group: { type: String }
+});
 module.exports = mongoose.model("GroceryItem", grocerySchema);
